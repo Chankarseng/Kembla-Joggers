4009f47cb2e6c66bffcc3d53b14008b28b1f36b7
Chan Kar seng <chankarseng96@gmail.com>
2018-02-27 17:49:29 +1100

    Added App Icon, and added some animation between ViewBoards

--- KemblaJoggers/KemblaJoggers/ViewController.designer.cs
+++ KemblaJoggers/KemblaJoggers/ViewController.designer.cs
@@ -13,8 +13,25 @@ namespace KemblaJoggers
     [Register ("ViewController")]
     partial class ViewController
     {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIImageView backgroundImage { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIImageView kemblaLogo { get; set; }
+
         void ReleaseDesignerOutlets ()
         {
+            if (backgroundImage != null) {
+                backgroundImage.Dispose ();
+                backgroundImage = null;
+            }
+
+            if (kemblaLogo != null) {
+                kemblaLogo.Dispose ();
+                kemblaLogo = null;
+            }
         }
     }
 }
